ebb9407aebf569e0037a830653dcee41aaddb411
Adrian Moldovan <adim.moldovan@gmail.com>
2020-04-30 17:02:40 +0300

    Updates

---
+++ precache-manifest.18b0d5aba0d936142d59f41eb680dcde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e1cf1a9f35f468ca9671d1b7c54cbdd",
+    "url": "/demo-shop/index.html"
+  },
+  {
+    "revision": "ad5e31563f8ae814d063",
+    "url": "/demo-shop/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5e14dc3d4252d9e11440",
+    "url": "/demo-shop/static/js/2.0bd2d591.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-shop/static/js/2.0bd2d591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad5e31563f8ae814d063",
+    "url": "/demo-shop/static/js/main.a40f38b7.chunk.js"
+  },
+  {
+    "revision": "7c6a8ad4c2f3481a7c21",
+    "url": "/demo-shop/static/js/runtime-main.ec54a67a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-shop/static/media/logo.5d5d9eef.svg"
+  }
+]);
