6c4075b5218c77e1dfe2663f6c3d7fa2689fa245
Adrian Moldovan <adim.moldovan@gmail.com>
2020-05-02 12:58:09 +0300

    Updates

--- precache-manifest.4983e12d0e02e38711d9cc9a691a42a2.js
+++ precache-manifest.4983e12d0e02e38711d9cc9a691a42a2.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "03ffa96808c36d9e8dd01613b8f31f7a",
+    "revision": "03d54386ed3a9666c61a57918078f37f",
     "url": "/demo-shop/index.html"
   },
   {
-    "revision": "9b1f66988c7c14981b7e",
+    "revision": "d8b03f6c2c0c932565d4",
     "url": "/demo-shop/static/css/2.69121389.chunk.css"
   },
   {
-    "revision": "3b690c6e72e4b43179a2",
+    "revision": "a3e0a49abd486719f489",
     "url": "/demo-shop/static/css/main.c8dddc80.chunk.css"
   },
   {
-    "revision": "9b1f66988c7c14981b7e",
-    "url": "/demo-shop/static/js/2.b4c1b818.chunk.js"
+    "revision": "d8b03f6c2c0c932565d4",
+    "url": "/demo-shop/static/js/2.5e955612.chunk.js"
   },
   {
     "revision": "ccfb27d768df50df1edb1a2bb04b68be",
-    "url": "/demo-shop/static/js/2.b4c1b818.chunk.js.LICENSE.txt"
+    "url": "/demo-shop/static/js/2.5e955612.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3b690c6e72e4b43179a2",
-    "url": "/demo-shop/static/js/main.3d0be7a1.chunk.js"
+    "revision": "a3e0a49abd486719f489",
+    "url": "/demo-shop/static/js/main.5d9a03ed.chunk.js"
   },
   {
     "revision": "7c6a8ad4c2f3481a7c21",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/demo-shop/precache-manifest.c274150cded05877b631c70cb3785e63.js"
+  "/demo-shop/precache-manifest.4983e12d0e02e38711d9cc9a691a42a2.js"
 );
 
 self.addEventListener('message', (event) => {
